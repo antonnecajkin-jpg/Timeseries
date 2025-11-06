7dd00ad0b4f0aa3a639bb8c6a0fb7627e70c5101
Anton Necajkin <antonnecajkin@gmail.com>
2025-11-06 12:20:57 +0300

    Ваше сообщение о проделанной работе

--- Script.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-  id INT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  age INT,
-  email VARCHAR(100),
-  country VARCHAR(50),
-  is_active BOOLEAN DEFAULT true,
-  created_at DATE NOT NULL
-);
